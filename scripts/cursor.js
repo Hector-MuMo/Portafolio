a2b2272f7559ee9626b11c283d21a4599edfde2a
Hector Muñoz <munozmoraleshector@gmail.com>
2021-06-03 23:43:15 -0500

    V1.0 Final :D

--- scripts/cursor.js
+++ scripts/cursor.js
@@ -2,7 +2,7 @@ const d = document;
 
 export default function followCursor() {
   const $circle = d.querySelector(".circle"),
-    $innerCircle = d.querySelector(".inner-circle");
+    $imgTitle = d.querySelector(".portfolio-img-title");
 
   d.addEventListener("mousemove", (e) => {
     if (
@@ -22,6 +22,21 @@ export default function followCursor() {
         "style",
         `top: ${e.pageY - 10}px; left: ${e.pageX - 15}px `
       );
+    }
+  });
+
+  d.addEventListener("mouseover", (e) => {
+    if (e.target.matches(".portfolio-img-container img")) {
+      $imgTitle.style.display = "inline-block";
+      $imgTitle.innerHTML = e.target.alt;
+      e.target.addEventListener("mousemove", (e) => {
+        $imgTitle.setAttribute(
+          "style",
+          `top: ${e.pageY + 20}px; left: ${e.pageX + 10}px `
+        );
+      });
+    } else {
+      $imgTitle.style.display = "none";
     }
   });
 }
